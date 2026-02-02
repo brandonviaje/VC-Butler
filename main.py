5c3596f045461591f1d9a7c1d873deb0c03056bb
brandonviaje <viajebrandon@gmail.com>
2026-02-02 15:55:45 -0500

    refactor: use faster-whisper for speech recognition

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
+from bot import Bot
 import discord
-import os
-from discord.ext import commands
-from discord import app_commands
+from discord.ext import voice_recv
 from dotenv import load_dotenv
+from faster_whisper import WhisperModel
+import logging
+import os
+from discord.ext.voice_recv import BasicSink
 
-load_dotenv() # load env variables
+# schedule async on_speech coroutine
+def on_speech_wrapper(recognizer, audio, user):
+    bot.loop.create_task(bot.on_speech(recognizer, audio, user))
 
-class Bot(commands.Bot):
+if __name__ == "__main__":
+    # load env variables, model, logger
+    load_dotenv() 
+    whisper_model = WhisperModel("base.en", device="cpu", download_root="./models", compute_type="int8")
+    logging.getLogger("discord.ext.voice_recv.reader").setLevel(logging.ERROR)
 
-    def __init__(self):
-        # intents config
-        intents = discord.Intents.default()
-        intents.message_content = True 
-        intents.voice_states = True
-        super().__init__(command_prefix="!", intents=intents)
-        
-    async def on_ready(self):
-        print(f"Logged on as {self.user}!")
-        guild = discord.Object(id=1467314035585450155)
+    # init bot and guild ID 
+    bot = Bot(whisper_model)
+    GUILD_ID = discord.Object(id=1467314035585450155)
 
-        # sync commands to dev server
-        try:
-            synced = await self.tree.sync(guild=guild)
-            print(f'Synced {len(synced)} commands to guild {guild.id}')
-        except Exception as e:
-            print(f'ERROR Syncing Commands: {e}')
+    # commands
+    @bot.tree.command(name="listen", description="Join call and give me commands!", guild=GUILD_ID)
+    async def listen(interaction: discord.Interaction):
+        if interaction.user.voice:
+            await interaction.response.defer()
+            vc = await interaction.user.voice.channel.connect(cls=voice_recv.VoiceRecvClient)
+            
+            def simple_callback(user, voice_data):
+                bot.loop.create_task(bot.on_speech(None, voice_data, user))
 
-    async def on_message(self, message):
-        # ignore itself
-        if message.author == self.user:
-            return
-        
-        await self.process_commands(message)
+            vc.listen(BasicSink(simple_callback)) 
+            
+            await interaction.followup.send("I am now listening to you!")
+        else:
+            await interaction.response.send_message("You need to be in a VC first!", ephemeral=True)
 
-# init bot and guild ID 
-bot = Bot()
-GUILD_ID = discord.Object(id=1467314035585450155)
-
-# slash commands
-@bot.tree.command(name="hello", description="Say Wassup", guild=GUILD_ID)
-async def sayHello(interaction : discord.Interaction):
-    await interaction.response.send_message("Sup Twin")
-
-@bot.tree.command(name="echo", description="Echo what you type", guild=GUILD_ID)
-async def echo(interaction : discord.Interaction, message : str):
-    await interaction.response.send_message(message)
-
-bot.run(os.getenv('DISCORD_TOKEN')) # run bot
+    bot.run(os.getenv('DISCORD_TOKEN')) # run bot
